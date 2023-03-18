ca65e6c9c0b0dca1aa32618a295641f32ee45b89
ppoliani <ppoliani@gmail.com>
2023-03-18 13:09:21 +0200

    feat: add fireabse auth package

---
+++ packages/firebase-auth/lib/index.js
@@ -0,0 +1,65 @@
+import Record from '@ppoliani/im-record'
+import {initializeApp} from 'firebase/app'
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  TwitterAuthProvider,
+  FacebookAuthProvider,
+  OAuthProvider
+} from 'firebase/auth'
+import jwt_decode from "jwt-decode";
+export * as Auth from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzQHN4AyJvbQ4hHogiDQGo0847Hj9HMng",
+  authDomain: "ticketland-30727.firebaseapp.com",
+  projectId: "ticketland-30727",
+  storageBucket: "ticketland-30727.appspot.com",
+  messagingSenderId: "711003129445",
+  appId: "1:711003129445:web:76576072c268d14ec2288c",
+  measurementId: "G-FLNYLRX66Y"
+};
+
+const providers = {
+  google: new GoogleAuthProvider(),
+  twitter: new TwitterAuthProvider(),
+  facebook: new FacebookAuthProvider(),
+  apple: new OAuthProvider('apple.com')
+}
+
+const signInWithGoogle = async () => await signInWithPopup(auth, providers.google)
+const signInWithTwitter = async () => await signInWithPopup(auth, providers.twitter)
+const signInWithFacebook = async () => await signInWithPopup(auth, providers.facebook)
+const signInWithApple = async () => await signInWithPopup(auth, providers.apple)
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+const accessToken = async () => {
+  const now = Date.now() / 1000
+  const token = jwt_decode(auth.currentUser.accessToken)
+
+  if (now > token.exp) {
+    await auth.currentUser.getIdToken(true)
+  }
+
+  return auth.currentUser.accessToken
+}
+
+const onUserChanged = (_, ...args) => auth.onIdTokenChanged(...args)
+const signOutUser = () => signOut(auth)
+
+const Auth = Record({
+  // Auth interface
+  accessToken,
+  onUserChanged,
+  signOutUser,
+  signInWithGoogle,
+  signInWithTwitter,
+  signInWithFacebook,
+  signInWithApple
+})
+
+export default Auth
